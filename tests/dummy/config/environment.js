c68802783f195d678dcf87fff28b3d391482d044
Glanzer <sglanzer@onm-sglanzer-01.ciena.com>
2016-02-09 10:31:57 -0500

    Fixing the production baseURL property

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.baseURL: '/ember-frost-theme'
+    ENV.baseURL = '/ember-frost-theme';
   }
 
   return ENV;
