408b1d4369fc873b83bbf24e091b36519a9280ab
Matthew Dahl <mdahl@ciena.com>
2016-03-17 09:14:50 -0700

    fix lint errors

--- tests/dummy/app/pods/components/demo-palette/component.js
+++ tests/dummy/app/pods/components/demo-palette/component.js
@@ -1,13 +1,18 @@
+/* global $ */
 import Ember from 'ember'
 
-function rgb2hex(rgb) {
-    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+function hex (x) {
+  return (`0${parseInt(x).toString(16).slice(-2)}`)
+}
 
-    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-    function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-    }
-    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+function rgb2hex (rgb) {
+  if (/^#[0-9A-F]{6}$/i.test(rgb)) {
+    return rgb
+  }
+
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)
+
+  return `#${hex(rgb[1])}${hex(rgb[2])}${hex(rgb[3])}`
 }
 
 export default Ember.Component.extend({
